853d5035192937a62cefaf24f6d2ef3f61f20332
idward <idward@vip.sina.com>
2019-01-26 19:39:06 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const {ApolloServer, gql} = require('apollo-server');
+
+const todos = [
+    {task: 'Go shopping', completed: false},
+    {task: 'Sleeping', completed: true}
+];
+
+const typeDefs = gql`
+    type Todo {
+        task:String,
+        completed:Boolean
+    }
+    type Query {
+        getTodos:[Todo]
+    }
+    type Mutation {
+        addTodo(task:String,completed:Boolean):Todo
+    }
+`;
+
+const resolvers = {
+    Query: {
+        getTodos() {
+            return todos;
+        }
+    },
+    Mutation: {
+        addTodo(_, args) {
+            let newTodo = {task: args.task, completed: args.completed};
+            todos.push(newTodo);
+            return newTodo;
+        }
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen(4500).then(({url}) => {
+    console.log(`Server is listening on ${url}`);
+});
